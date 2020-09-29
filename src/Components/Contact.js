27dc60320975b0c6e0af493b5d34dfb6c23d11ee
Sambhav Jain <sambhav@xencov.com>
2020-09-29 16:05:27 +0530

    removed contact form

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -24,28 +24,7 @@ function Contact(props) {
 
         <div className="row">
           <div className="eight columns">
-            <form action="https://getsimpleform.com/messages?form_api_token=1194ca1ac74480ae6a5c56735b4e6b71"
-                  method="post">
-              <input type='hidden'/>
-              <div>
-                <label htmlFor="name">Name <span className="required">*</span></label>
-                <input type="text" defaultValue="" size="35" id="name" name="name" required/>
-              </div>
-              <div>
-                <label htmlFor="email">Email <span className="required">*</span></label>
-                <input type="email" defaultValue="" size="35" id="email" name="email" required/>
-              </div>
-              <div>
-                <label htmlFor="subject">Subject <span className="required">*</span></label>
-                <input type="text" defaultValue="" size="35" id="subject" name="subject" required/>
-              </div>
-              <div>
-                <label htmlFor="message">Message <span className="required">*</span></label>
-                <textarea cols="50" rows="15" id="message" name="message" required/>
-              </div>
 
-              <button className="submit" type="submit">Submit</button>
-            </form>
           </div>
 
           <aside className="four columns footer-widgets">
