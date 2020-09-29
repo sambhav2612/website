e1bdc6fcc9923714d2f4483f62e0cf6b23dca06f
Sambhav Jain <sambhav@xencov.com>
2020-09-29 15:31:03 +0530

    integrated contact form

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,50 +1,6 @@
-import React, {useState} from 'react';
+import React from 'react';
 
 function Contact(props) {
-  const [nameF, setName] = useState('');
-  const [emailF, setEmail] = useState('');
-  const [subjectF, setSubject] = useState({});
-  const [messageF, setMessage] = useState('');
-  const [status, setStatus] = useState('');
-  const [error, setError] = useState('');
-
-  const encode = (data) => {
-    const formData = new FormData();
-    Object.keys(data).forEach((k) => {
-      formData.append(k, data[k])
-    });
-    return formData;
-  }
-
-  const handleSubmit = e => {
-    const data = {"form-name": "contact", name: nameF, email: emailF, subject: subjectF, message: messageF}
-
-    fetch("/", {
-      method: "POST",
-      body: encode(data)
-    })
-        .then(() => setStatus("Your message was sent, thank you!"))
-        .catch(error => setError("Form Submission Failed!"));
-
-    e.preventDefault();
-  };
-
-  const handleChange = e => {
-    const {name, value} = e.target
-    if (name === 'name') {
-      return setName(value);
-    }
-    if (name === 'email') {
-      return setEmail(value);
-    }
-    if (name === 'subject') {
-      return setSubject(value);
-    }
-    if (name === 'message') {
-      return setMessage(value);
-    }
-  }
-
   if (props.data) {
     var name = props.data.name;
     var street = props.data.address.street;
@@ -68,35 +24,28 @@ function Contact(props) {
 
         <div className="row">
           <div className="eight columns">
-            <form onSubmit={handleSubmit} name="contactForm">
-              <input type="hidden" name="contactForm" value="contact"/>
+            <form action="https://getsimpleform.com/messages?form_api_token=1194ca1ac74480ae6a5c56735b4e6b71"
+                  method="post">
+              <input type='hidden'/>
               <div>
                 <label htmlFor="name">Name <span className="required">*</span></label>
-                <input type="text" defaultValue="" size="35" id="name" name="name" required onChange={handleChange}/>
+                <input type="text" defaultValue="" size="35" id="name" name="name" required/>
               </div>
               <div>
                 <label htmlFor="email">Email <span className="required">*</span></label>
-                <input type="email" defaultValue="" size="35" id="email" name="email" required onChange={handleChange}/>
+                <input type="email" defaultValue="" size="35" id="email" name="email" required/>
               </div>
               <div>
                 <label htmlFor="subject">Subject <span className="required">*</span></label>
-                <input type="text" defaultValue="" size="35" id="subject" name="subject" required
-                       onChange={handleChange}/>
+                <input type="text" defaultValue="" size="35" id="subject" name="subject" required/>
               </div>
               <div>
                 <label htmlFor="message">Message <span className="required">*</span></label>
-                <textarea cols="50" rows="15" id="message" name="message" required onChange={handleChange}/>
+                <textarea cols="50" rows="15" id="message" name="message" required/>
               </div>
 
               <button className="submit" type="submit">Submit</button>
             </form>
-
-            {error && <div id="message-warning">{error}</div>}
-            {status && <div id="message-success">
-              <i className="fa fa-check"/>
-              {status}
-              <br/>
-            </div>}
           </div>
 
           <aside className="four columns footer-widgets">
